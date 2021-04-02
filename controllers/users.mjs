c309c67aad754a04d2e18b8f7db44926493442e7
jeremylim91 <jeremylim_91@live.com>
2021-04-03 00:46:05 +0800

    added code to get all user name and table id

--- controllers/users.mjs
+++ controllers/users.mjs
@@ -1,44 +1,16 @@
 export default function initUsersController(db) {
-  const index = async (request, response) => {
+  const index = async (req, res) => {
     try {
-      const orders = await db.Order.findAll();
-      response.send({ orders });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const create = async (request, response) => {
-    try {
-      const order = await db.Order.create({
-        total: request.body.total,
+      const allUsers = await db.User.findAll({
+        attributes: ['id', 'username'],
       });
-
-      const { items } = request.body;
-
-      const orderItemQueries = [];
-      for (let i = 0; i < items.length; i += 1) {
-        const item = {
-          order_id: order.id,
-          item_id: items[i].id,
-          quantity: items[i].quantity,
-          created_at: new Date(),
-          updated_at: new Date(),
-        };
-
-        orderItemQueries.push(db.OrderItem.create(item));
-      }
-
-      const orderItemResults = await Promise.all(orderItemQueries);
-
-      response.send({ orderItems: orderItemResults, order });
+      res.send(allUsers);
     } catch (error) {
       console.log(error);
     }
   };
 
   return {
-    create,
     index,
   };
 }
