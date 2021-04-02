4bf5d28fc139cf497386a4d700a6b58e0f9b8933
jeremylim91 <jeremylim_91@live.com>
2021-04-03 00:44:49 +0800

    added code to save booking to db

--- controllers/bookings.mjs
+++ controllers/bookings.mjs
@@ -1,15 +1,44 @@
 export default function initBookingsController(db) {
-  // display all the rooms for user to see
-  const index = async (request, response) => {
+  console.log('inside initBookingsController');
+
+  // display all the bookings related to a specific room
+  const index = async (req, res) => {
+    const { roomId } = req.params;
     try {
-      const rooms = await db.Room.findAll();
-      response.send({ rooms });
+      const bookings = await db.Booking.findAll();
+      res.send(bookings);
     } catch (error) {
       console.log(error);
     }
   };
+  const add = async (req, res) => {
+    const {
+      roomId, startTime, endTime, agenda, attendees,
+    } = req.body.meetingDetails;
 
+    console.log('attendees is:');
+    console.log(attendees);
+    try {
+      const newBookingInstance = await db.Booking.create({
+        userId: 1, /* Update this after login implemented */
+        roomId,
+        startTime,
+        endTime,
+        agenda,
+      });
+      console.log('newBookingInstance is:');
+      console.log(newBookingInstance);
+      attendees.forEach((attendee) => {
+        // db.User.addMeetings([newBookingInstance, attendee.id]);
+        newBookingInstance.addUser(attendee.id);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    res.send();
+  };
   return {
     index,
+    add,
   };
 }
--- routes.mjs
+++ routes.mjs
@@ -3,18 +3,18 @@ import db from './models/index.mjs';
 
 import initRoomsController from './controllers/rooms.mjs';
 import initUsersController from './controllers/users.mjs';
-import initBookingsController from './controllers/users.mjs';
+import initBookingsController from './controllers/bookings.mjs';
 
 export default function routes(app) {
   const UsersController = initUsersController(db);
-  // app.post('/orders', OrdersController.create);
-  // app.get('/orders', OrdersController.index);
+  app.get('/users/findAll', UsersController.index);
 
   const RoomsController = initRoomsController(db);
   app.get('/rooms', RoomsController.index);
 
   const BookingsController = initBookingsController(db);
-  // app.get('/rooms', RoomsController.index);
+  app.get('/bookings/:roomId', BookingsController.index);
+  app.post('/bookings', BookingsController.add);
 
   // special JS page. Include the webpack index.html file
   app.get('/home', (request, response) => {
