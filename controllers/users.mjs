f34322ea0c3aec4493f3b84ebe76d2139cd0d5cd
jeremylim91 <jeremylim_91@live.com>
2021-04-03 08:54:25 +0800

    amended index to narrow search to exclude users who have is_deleted==false

--- controllers/users.mjs
+++ controllers/users.mjs
@@ -2,6 +2,9 @@ export default function initUsersController(db) {
   const index = async (req, res) => {
     try {
       const allUsers = await db.User.findAll({
+        where: {
+          is_deleted: false,
+        },
         attributes: ['id', 'username'],
       });
       res.send(allUsers);
@@ -9,8 +12,16 @@ export default function initUsersController(db) {
       console.log(error);
     }
   };
+  const deleteUser = async (req, res) => {
+    try {
+      const deleteUser = await db.User.findByPk(req.body.userId);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return {
     index,
+    deleteUser,
   };
 }
