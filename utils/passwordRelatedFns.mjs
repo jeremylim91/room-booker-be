2a721ea988670e7e720688bcf1806cb3d296f019
jeremylim91 <jeremylim_91@live.com>
2021-04-06 01:07:12 +0800

    refactored the code

--- utils/passwordRelatedFns.mjs
+++ utils/passwordRelatedFns.mjs
@@ -1,8 +1,20 @@
 import jsSHA from 'jssha';
 
-export default function getHashedString(stringToHash) {
+const SALT = 'hello';
+
+// Fn that converts a variable into a hashed string
+export function getHashedString(stringToHash) {
   const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
   shaObj.update(stringToHash);
   const hashedString = shaObj.getHash('HEX');
   return hashedString;
+}
+
+// Fn that converts supplied userId into a hash (using a salt)
+export function convertUserIdToHash(userId) {
+  const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
+  const unhashedCookieString = `${userId}-${SALT}`;
+  shaObj.update(unhashedCookieString);
+  const hashedCookieString = shaObj.getHash('HEX');
+  return hashedCookieString;
 }
