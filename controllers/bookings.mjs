d85a39d56677ec70eff38a330b788bf7ec60726b
jeremylim91 <jeremylim_91@live.com>
2021-04-08 22:04:40 +0800

    add code to allow admin and booking owner to update booking

--- controllers/bookings.mjs
+++ controllers/bookings.mjs
@@ -118,6 +118,47 @@ export default function initBookingsController(db) {
       console.log(error);
     }
   };
+  const updateBooking = async (req, res) => {
+  //  do a check to see if user is an admin, or is the ownwer of this booking
+
+    const { bookingId, agendaInputField: updatedAgenda, tagsProp: updatedAttendees } = req.body;
+    // get the userId from cookies
+    let { loggedInUserId } = req.cookies;
+    loggedInUserId = parseInt(loggedInUserId, 10);
+
+    try {
+      // get a user instanced based on the userId
+      const userInstance = await db.User.findByPk(loggedInUserId);
+
+      // get a booking using the bookingId
+      const bookingInstance = await db.Booking.findByPk(bookingId);
+
+      // if user is not an admin and is not the room booker, don't let him cancel the booking
+      if (userInstance.isAdmin === false && bookingInstance.userId !== loggedInUserId) {
+        console.log('user not allowed becos does not have credentials to delete');
+        res.send('disallow');
+        return;
+      }
+      // update the bookings table that the agenda has changed
+      bookingInstance.agenda = updatedAgenda;
+      console.log(bookingInstance);
+      await bookingInstance.save();
+
+      // update the thru table to remove attendees
+      bookingInstance.removeAttendee();
+
+      updatedAttendees.forEach((attendee) => {
+        bookingInstance.addAttendee(attendee.id);
+      });
+
+      // save changes to the db
+      await bookingInstance.save();
+
+      res.send();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return {
     index,
@@ -126,5 +167,6 @@ export default function initBookingsController(db) {
     bookingsByRoomId,
     getMtgAttendees,
     deleteABooking,
+    updateBooking,
   };
 }
