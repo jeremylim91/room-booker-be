fa9b7b107c06a0a12d378a55006c3c95c90bda98
jeremylim91 <jeremylim_91@live.com>
2021-04-14 02:06:11 +0800

    added env variables to enable creation of db within docker container

--- config/config.js
+++ config/config.js
@@ -1,9 +1,17 @@
 module.exports = {
+  // development: {
+  //   username: process.env.USER,
+  //   password: null,
+  //   database: 'room_booker_development',
+  //   host: '127.0.0.1',
+  //   dialect: 'postgres',
+  // },
+
   development: {
-    username: process.env.USER,
-    password: null,
-    database: 'room_booker_development',
-    host: '127.0.0.1',
+    username: process.env.DB_USER || 'jeremylim',
+    password: process.env.DB_PASSWORD|| null,
+    database: process.env.DB_NAME || 'room_booker_development',
+    host: process.env.DB_HOST||'localhost',
     dialect: 'postgres',
   },
   production: {
